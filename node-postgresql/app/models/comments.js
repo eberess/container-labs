72b326c766afebacc7c9e6ee011ed3b0ef5fcc47
ELB <el.beress@gmail.com>
2025-09-21 23:18:44 +0200

    Create comments.js

---
+++ node-postgresql/app/models/comments.js
@@ -0,0 +1,9 @@
+module.exports = (sequelize, DataTypes) => {
+  const Comment = sequelize.define('Comment', {
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+  });
+  return Comment;
+};
